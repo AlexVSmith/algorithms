613bd24cd16ee25e3459cbcdd2c6f15974472631
AlexVSmith <smith@it-interface.ru>
2018-02-06 17:03:10 +0500

    v1.0.6

--- src/main/java/com/kate/Main.java
+++ src/main/java/com/kate/Main.java
@@ -4,7 +4,7 @@ import javax.swing.UIManager;
 
 public class Main {
     public static void main(String[] args) {
-        String release = "2018-01-25 19:00";
+        String release = "2018-02-06 17:00";
 
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
